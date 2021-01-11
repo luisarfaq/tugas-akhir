ef9b49c58b82dda145e6a5acc6cf5ff7a5c37f17
luisarfaq <luisarfaq@gmail.com>
2021-01-11 21:26:34 +0700

    setting email

--- application/helpers/email_helper.php
+++ application/helpers/email_helper.php
@@ -2,14 +2,28 @@
 error_reporting(0);
 function kirim_email($email, $subject, $message)
 {
+
+
+    $is_hosting = $_SERVER['SERVER_NAME'] === 'localhost'?false:true;
+
+    if ($is_hosting) {
+        $smtp_host = 'mail.topgear-coffee.my.id';
+        $smtp_user = 'admin@topgear-coffee.my.id';
+        $smtp_pass = 'topgear-123';
+    }else{
+        $smtp_host = 'smtp.gmail.com';
+        $smtp_user = 'boyastro768@gmail.com';
+        $smtp_pass = 'Astroboy-111';
+    }
+
     $ci = get_instance();
     $ci->load->library('email');
     $config['protocol'] = "smtp";
-    $config['smtp_host'] = "smtp.gmail.com";
+    $config['smtp_host'] = $smtp_host;
     $config['smtp_crypto'] = "ssl";
     $config['smtp_port'] = "465";
-    $config['smtp_user'] = "boyastro768@gmail.com";
-    $config['smtp_pass'] = "Astroboy-111";
+    $config['smtp_user'] = $smtp_user;
+    $config['smtp_pass'] = $smtp_pass;
     $config['charset'] = "iso-8859-1";
     $config['mailtype'] = "html";
     $config['newline'] = "\r\n";
