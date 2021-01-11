a95d9453dd5eba7afce458adbc18f244040945eb
luisarfaq <luisarfaq@gmail.com>
2021-01-11 23:14:38 +0700

    Update view_detail_produk.php

--- application/views/home/produk/view_detail_produk.php
+++ application/views/home/produk/view_detail_produk.php
@@ -100,7 +100,7 @@ $produk = $row['nama_produk'];
             </div>
             <form id="product-form" class="product__options">
                 <?php
-                    $e_url = encrypt_url(21);//$row['id_produk']
+                    $e_url = encrypt_url(21)?encrypt_url(21):'kosong';//$row['id_produk']
                     echo $e_url;
                 ?>
                 <div class="form-group product__option">
