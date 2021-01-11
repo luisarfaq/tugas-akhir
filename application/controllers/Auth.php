f8b2f2643f65cfde8138c8a3b8ac6119a503887f
luisarfaq <luisarfaq@gmail.com>
2021-01-11 22:04:33 +0700

    Update Auth.php

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -230,7 +230,7 @@ class Auth extends CI_Controller
 				$cek = kirim_email($email, $subject, $message);
 				if ($cek) {
 					// $this->model_app->insert('tb_pengguna', $data);
-					$this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"><center>Silahkan cek email anda untuk aktivasi pendaftaran.</center></div>');
+					// $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"><center>Silahkan cek email anda untuk aktivasi pendaftaran.</center></div>');
 					redirect('login');
 					echo "login";
 				}else{
