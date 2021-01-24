7a36101794b84ede7bfbf009a023ca4207316fd2
luisarfaq <luisarfaq@gmail.com>
2021-01-24 16:55:31 +0700

    Update footer.php

--- application/views/home/_include/footer.php
+++ application/views/home/_include/footer.php
@@ -90,7 +90,7 @@
             </div>
 
             <div class="site-footer__bottom">
-                <div class="site-footer__copyright">&copy; <?= date('Y') ?> <a href="https://TopgearCoffee&Roastery.com/" target="_BLANK">Topgear Coffee & Roastery</a></div>
+                <div class="site-footer__copyright">&copy; <?= date('Y') ?> <a href="http://topgear-coffee.my.id/" target="_BLANK">Topgear Coffee & Roastery</a></div>
                <!--  <div class="site-footer__payments"><img src="<?= base_url('assets/template/tema/') ?>images/payments.png" alt=""></div>
             </div> -->
         </div>
